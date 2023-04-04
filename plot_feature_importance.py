e29e706baaca0d3dc5a40a9bf32d6ed2cdaf72f3
andygoliver <72929546+andygoliver@users.noreply.github.com>
2023-04-04 10:00:48 +0200

    added feature importance script

---
+++ plot_feature_importance.py
@@ -0,0 +1,76 @@
+import os
+import argparse
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import tensorflow_decision_forests as tfdf
+
+from prepare_data_BDT import select_feature_labels
+
+parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument(
+    "model_path",
+    type=str,
+    required=True,
+    help="Path to the model to process.",
+)
+parser.add_argument(
+    "fig_name",
+    type=str,
+    required=True,
+    help="Name of saved figure.",
+)
+
+def get_2d_importances(model_path: str, variable_importance:str, feature_choice: str = 'jedinet') -> np.ndarray:
+    feature_list = select_feature_labels(feature_choice)
+
+    inspector_path = os.path.join(model_path, "assets")
+    inspector = tfdf.inspector.make_inspector(inspector_path)
+
+    nb_features = len(feature_list)
+    # print(f'features per constituent: {nb_features}')
+    nb_constituents = int(len(inspector.features())/nb_features)
+    # print(f'total features: {len(inspector.features())}')
+    # print(f'constituents per jet: {nb_constituents}')
+    
+    feature_importance_dict = {i[0][0]: i[1] for i in inspector.variable_importances()[variable_importance]}
+
+    feature_importances  = np.zeros((nb_features, nb_constituents))
+    for feature_index in range(nb_features):
+        for constituent_index in range(nb_constituents):
+            try:
+                feature_importances[feature_index][constituent_index] = feature_importance_dict[f'c{constituent_index}_{feature_list[feature_index]}']
+                # print(f'c{constituent_index}_{feature_list[feature_index]} was found')
+
+            except:
+                # print(f'c{constituent_index}_{feature_list[feature_index]} was not found')
+                continue
+    
+    return feature_importances
+
+def plot_importances(model_path: str, variable_importance:str, feature_choice: str = 'jedinet', cmap: str = "YlGnBu",
+                     save_fig: bool = False, fig_name:str = '', fig_directory:str = 'Plots', show_fig:bool = False):
+    feature_importances = get_2d_importances(model_path, variable_importance, feature_choice = feature_choice)
+
+    # plt.subplots(figsize=(18.75,5))
+    ax = sns.heatmap(feature_importances, linewidth=0.5, cmap=cmap, yticklabels = select_feature_labels(feature_choice), square = True)
+    ax.set(xlabel='Constituent', ylabel = 'Feature', title=f'{variable_importance} feature importance')
+    plt.tight_layout()
+    if save_fig:
+        savepath = os.path.join(fig_directory, f"{variable_importance}_{fig_name}.pdf")
+        plt.savefig(savepath)
+    if show_fig:
+        plt.show()
+    
+    plt.close()
+
+args = parser.parse_args()
+importances  = ['INV_MEAN_MIN_DEPTH', 'NUM_AS_ROOT', 'NUM_NODES', 'SUM_SCORE']
+
+for importance in importances:
+    print(importance)
+    plot_importances(args.model_path, 
+                     importance, show_fig = False,
+                     save_fig = True, fig_name = args.fig_name)
