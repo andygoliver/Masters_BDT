569ce3c3ac51d02dec9f649df5196e310662f8dc
Andrew Oliver (ETHZ) [aoliver] <aoliver@t3ui01.psi.ch>
2023-04-14 16:41:46 +0200

    added binary class in evaluation

--- BDT_train.sh
+++ BDT_train.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-#SBATCH --job-name=BDTagg
+#SBATCH --job-name=BDTnoagg
 #SBATCH --account=t3
 #SBATCH --partition=standard
-#SBATCH --mem=30000
+#SBATCH --mem=20000
 #SBATCH --time=0-12:00
 #SBATCH --output=./training_logs/BDT_data_cpu_%j.out
 
 # Folder where the data is located for the training and testing of the BDT.
 # Change so it suits your configuration.
-data_path_train="/work/aoliver/Data/binary_class/jet_images_c50_sort_hpT_pct_agg_train.csv"
+data_path_train="/work/aoliver/Data/binary_class/jet_images_c16_sort_hpT_pct_noagg_train.csv"
 # data_path_test="/work/aoliver/Data/jet_images_c8_pt2.0_andre_test.csv"
 
 # Default parameters
 # nb_jets=None
 model_output_dir=/work/aoliver/BDT/Models/Binary_class/
-model_name=BDT_c50_hpT_pct_agg
+model_name=BDT_c16_hpT_pct_noagg
 
 # Gather parameters given by user.
 for ARGUMENT in "$@"
--- evaluate_model.py
+++ evaluate_model.py
@@ -5,6 +5,7 @@ import pandas as pd
 import argparse
 import time
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import tensorflow_decision_forests as tfdf
 from tensorflow.keras.models import load_model
@@ -30,81 +31,114 @@ parser.add_argument(
 parser.add_argument(
     "plot_name", 
     type=str, 
-    help="Directory to save the plots in."
+    help="Name of the saved plot."
+)
+parser.add_argument(
+    "--classification_type", 
+    type=str,
+    default='binary',
+    choices = ['multiclass', 'binary'], 
+    help="Type of classification performed by the model."
 )
 
-args = parser.parse_args()
+def main(args):
+    test_starting_time = time.time()
+    test_df = pd.read_csv(args.data_path_test, nrows = None)
+    test_load_time = time.time()-test_starting_time
+    print(f'Loaded testing sample of {len(test_df)} jets in {test_load_time:.3f}s')
 
-# define classes for the labels (second array is quick fix to bytes problem)
-# believe that the model spits the predictions out in alphabetical order
-classes = np.array(['g', 'q', 't', 'w', 'z'])
-bclasses = np.array(["b'g'", "b'q'", "b't'", "b'w'", "b'z'"])
+    test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(test_df, label='class')
+    model = load_model(args.model_path)
 
-test_starting_time = time.time()
-test_df = pd.read_csv(args.data_path_test, nrows = None)
-test_load_time = time.time()-test_starting_time
-print(f'Loaded testing sample of {len(test_df)} jets in {test_load_time:.3f}s')
+    model.compile(metrics=["accuracy"])
+    evaluation = model.evaluate(test_ds, return_dict=True)
 
-test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(test_df, label='class')
-model = load_model(args.model_path)
+    inspector_path = os.path.join(args.model_path, "assets")
+    inspector = tfdf.inspector.make_inspector(inspector_path)
 
-model.compile(metrics=["accuracy"])
-evaluation = model.evaluate(test_ds, return_dict=True)
+    y_pred = model.predict(test_ds)
+    y_test = test_df['class']
 
-inspector_path = os.path.join(args.model_path, "assets")
-inspector = tfdf.inspector.make_inspector(inspector_path)
+    if args.classification_type == 'multiclass':
+        make_multiclass_ROC(y_pred, y_test, args.plot_name, args.plot_output_dir, evaluation)
 
-y_pred = model.predict(test_ds)
-# print(y_pred)
-y_test = test_df['class']
-# print(y_test)
+    else:
+        make_binary_ROC(y_pred, y_test, args.plot_name, args.plot_output_dir, evaluation)
 
-fpr = {}
-tpr = {}
-aucs = {}
 
-for i, label in enumerate(classes):
-    print(f'{[int(y) for y in y_test == bclasses[i]][:5]}, {[int(y) for y in y_test == bclasses[i]][-5:]}')
-    print(f'{y_pred[:,i][:5]}, {y_pred[:,i][-5:]}')
-    fpr[label], tpr[label], threshold = roc_curve([int(y) for y in y_test == bclasses[i]],y_pred[:,i])
-    aucs[label] = auc(fpr[label],tpr[label])
+    print('\n=============================================\n')
+
+    print('Inputs')
+    print('------')
+
+    print(f'Evaluated model: {args.model_path}')
+    print(f'Test dataset: {args.data_path_test}')
+
+    print('\nOutputs')
+    print('-------')
+
+    print(f'ROC saved as: {args.plot_output_dir + args.plot_name}')
+
+    print('\nModel information')
+    print('--------------------')
+        
+    print("model type:", inspector.model_type())
+    print("number of trees:", inspector.num_trees())
+    print("objective:", inspector.objective())
+    #print("Input features:", inspector.features())
+
+    for name, value in evaluation.items():
+        print(f"{name}: {value:.4f}")
+
+    print('\n=============================================\n')
+
+def make_binary_ROC(y_pred, y_test, plot_name, plot_output_dir, evaluation):
+    fpr, tpr, threshold = roc_curve(y_test,y_pred)
+    auc_ = auc(fpr,tpr)
     #print('\n--------------------------------\n')
 
-fig, ax = plt.subplots(figsize=(9, 9))
-for i, label in enumerate(classes):
-    plt.plot(tpr[label],fpr[label], label = f'{label} tagger, AUC = {aucs[label]*100:.1f}%')    
-plt.semilogy()
-plt.xlabel("Signal Efficiency")
-plt.ylabel("Background Efficiency")
-plt.ylim(0.001,1)
-plt.grid(True)
-plt.legend(loc='upper left')
-plt.figtext(0.25, 0.90,f"Accuracy = {evaluation['accuracy']:.4f}",fontweight='bold', wrap=True, horizontalalignment='right', fontsize=14)
-plt.savefig(os.path.join(args.plot_output_dir, args.plot_name))
-plt.close()
+    fig, ax = plt.subplots(figsize=(9, 9))
+    plt.plot(tpr,fpr, label = f't tagger, AUC = {auc_*100:.1f}%')    
+    plt.semilogy()
+    plt.xlabel("Signal Efficiency")
+    plt.ylabel("Background Efficiency")
+    plt.ylim(0.001,1)
+    plt.grid(True)
+    plt.legend(loc='upper left')
+    plt.figtext(0.25, 0.90,f"Accuracy = {evaluation['accuracy']:.4f}",fontweight='bold', wrap=True, horizontalalignment='right', fontsize=14)
+    plt.savefig(os.path.join(plot_output_dir, plot_name))
+    plt.close()
 
-print('\n=============================================\n')
+def make_multiclass_ROC(y_pred, y_test, plot_name, plot_output_dir, evaluation):
+    # define classes for the labels (second array is quick fix to bytes problem)
+    # believe that the model spits the predictions out in alphabetical order
+    classes = np.array(['g', 'q', 't', 'w', 'z'])
+    bclasses = np.array(["b'g'", "b'q'", "b't'", "b'w'", "b'z'"])
 
-print('Inputs')
-print('------')
+    fpr = {}
+    tpr = {}
+    aucs = {}
 
-print(f'Evaluated model: {args.model_path}')
-print(f'Test dataset: {args.data_path_test}')
+    for i, label in enumerate(classes):
+        print(f'{[int(y) for y in y_test == bclasses[i]][:5]}, {[int(y) for y in y_test == bclasses[i]][-5:]}')
+        print(f'{y_pred[:,i][:5]}, {y_pred[:,i][-5:]}')
+        fpr[label], tpr[label], threshold = roc_curve([int(y) for y in y_test == bclasses[i]],y_pred[:,i])
+        aucs[label] = auc(fpr[label],tpr[label])
+        #print('\n--------------------------------\n')
 
-print('\nOutputs')
-print('-------')
+    fig, ax = plt.subplots(figsize=(9, 9))
+    for i, label in enumerate(classes):
+        plt.plot(tpr[label],fpr[label], label = f'{label} tagger, AUC = {aucs[label]*100:.1f}%')    
+    plt.semilogy()
+    plt.xlabel("Signal Efficiency")
+    plt.ylabel("Background Efficiency")
+    plt.ylim(0.001,1)
+    plt.grid(True)
+    plt.legend(loc='upper left')
+    plt.figtext(0.25, 0.90,f"Accuracy = {evaluation['accuracy']:.4f}",fontweight='bold', wrap=True, horizontalalignment='right', fontsize=14)
+    plt.savefig(os.path.join(plot_output_dir, plot_name))
+    plt.close()
 
-print(f'ROC saved as: {args.plot_output_dir + args.plot_name}')
-
-print('\nModel information')
-print('--------------------')
-    
-print("model type:", inspector.model_type())
-print("number of trees:", inspector.num_trees())
-print("objective:", inspector.objective())
-#print("Input features:", inspector.features())
-
-for name, value in evaluation.items():
-  print(f"{name}: {value:.4f}")
-
-print('\n=============================================\n')
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(args)
